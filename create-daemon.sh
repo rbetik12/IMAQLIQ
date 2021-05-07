72a8ee6e4d52f6bf07f5db9a23b21a216b08358a
Vitaliy <mr.prikota@gmail.com>
2021-05-07 16:16:35 +0300

    Added daemon creating script

---
+++ create-daemon.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+sudo chmod +x ./server
+sudo touch /etc/systemd/system/cooldaemon.service
+sudo echo "
+[Unit]
+Description=Cool daemon
+
+[Service]
+ExecStart=$PWD/server 8080
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+" > /etc/systemd/system/cooldaemon.service
